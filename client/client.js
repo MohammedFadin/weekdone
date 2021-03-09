259feadd77716d08b445625866c5eabcdb3999ac
Mohammed Fadin <imohammad@me.com>
2021-03-09 14:40:44 +0400

    IMP Add Configmap to Helm chart + Remove .env file

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require('body-parser')
-const dotenv = require('dotenv').config()
+// const dotenv = require('dotenv').config()
 const axios = require("axios")
 const health = require("@cloudnative/health-connect")
 const backend_endpoint = process.env.BACKEND_URL+':'+process.env.BACKEND_PORT
